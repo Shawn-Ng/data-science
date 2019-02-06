039625b946d8517b82b666f2629d3e65ea4aead9
anonymous <shawn.coding.acc@gmail.com>
2019-02-06 08:24:38 +0800

    Create commonly-used functions file

---
+++ pandas/pandas_common.py
@@ -0,0 +1,175 @@
+def config_pandas_display():
+    '''
+    Configure Pandas display
+    '''
+    import pandas as pd
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.max_colwidth', -1)
+    pd.set_option('display.max_rows', 200)
+    pd.set_option('display.expand_frame_repr', True)
+
+
+def reset_pandas_display():
+    '''
+    Reset Pandas display
+    '''
+    import pandas as pd
+    pd.set_option('display.max_columns', 20)
+    pd.set_option('display.max_colwidth', 50)
+    pd.set_option('display.max_rows', 60)
+    pd.set_option('display.expand_frame_repr', True)
+
+
+# PURE PYTHON FUNCTION
+def clean_string_literal(aStr):
+    '''
+    Convert literal string to string
+    
+    Parameter
+    ---------
+    aStr: str
+        The column to convert from string literal to string
+    
+    Return
+    ------
+    Cleansed string
+    '''
+    result = aStr
+    
+    if isinstance(aStr, (str)):
+        result = aStr.strip("'\"")
+    
+    return result
+
+
+def convert_string_to_number(aStr, numericType=''):
+    '''
+    Convert string number to number (int / float / bool)
+    
+    Parameter
+    ---------
+    aStr: str
+        The column to convert from string to number
+    
+    Return
+    ------
+    Number
+    '''
+    result = aStr
+
+    if isinstance(aStr, (str)):
+        if numericType == 'int':
+            result = int(clean_string_literal(aStr))
+
+        if numericType == 'float':
+            result = float(clean_string_literal(aStr))
+        
+    return result
+
+
+def convert_string_to_boolean(aStr, booleanType='bool'):
+    '''
+    Convert string boolean to boolean (bool / int)
+    
+    Parameter
+    ---------
+    aStr: str
+        The column to convert from string to boolean
+    
+    Note
+    ----
+    Do not use eval() as it's unsafe
+    
+    Return
+    ------
+    Boolean or integer
+    '''
+    import distutils.util
+
+    result = aStr
+    
+    if booleanType == 'bool':
+        if isinstance(aStr, (str)):
+            # result = eval(clean_string_literal(aStr))
+            result = bool(distutils.util.strtobool(clean_string_literal(aStr)))
+
+    if booleanType == 'int':
+        if isinstance(aStr, (bool)):
+            result = int(aStr)
+        if isinstance(aStr, (str)):
+            # result = int(eval(clean_string_literal(aStr)))
+            result = distutils.util.strtobool(clean_string_literal(aStr))
+    
+    return result
+
+
+def convert_string_to_DL(aStr):
+    '''
+    Convert string dictionary/list to dictionary/list
+        
+    Parameter
+    ---------
+    aStr: str
+        The column to convert from string to dictionary/list
+    
+    Return
+    ------
+    Dictionary / list
+    '''
+    import ast
+
+    result = aStr
+    
+    if isinstance(aStr, (str)):
+        result = ast.literal_eval(clean_string_literal(aStr))
+    
+    return result
+    
+
+# PANDAS FUNCTION
+def add_type_columns(pandasDf):
+    '''
+    Return Pandas dataframe where each columns has their type column
+    
+    Parameter
+    ---------
+    pandasDf: pandas.core.frame.DataFrame
+        One Pandas dataframe
+    
+    Return
+    ------
+    A Pandas dataframe
+    '''
+    suffix = '_type'
+    for column in pandasDf.columns:
+        if not column.endswith(suffix):
+            newColumn = column+suffix
+            pandasDf[newColumn] = pandasDf[column].apply(lambda x: type(x))
+    return pandasDf
+
+
+def compare_all_list_items(aList):
+    '''
+    Print every comparison between list's 2 items
+    
+    Parameter
+    ---------
+    aList: list
+        A list
+
+    Return
+    ------
+    A Pandas dataframe
+    '''
+    import itertools
+    import pandas as pd
+    
+    result = []
+    
+    for a, b in itertools.combinations(aList, 2):
+        # print(f"{repr(a)} == {repr(b)}: {a==b}")
+        row = {'item1':repr(a), 'item2':repr(b), 'comparison':a==b}
+        result.append(row)
+
+    df = pd.DataFrame(result, columns=['item1','item2','comparison'])
+    return df
